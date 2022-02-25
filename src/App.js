cd30d4f6f45aa6cfab20a7aef6a902fedc363f09
thanhnguyen98 <ngtruongthanh98@gmail.com>
2022-02-25 10:29:42 +0700

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.scss';
 import LoginPage from './pages/Login';
 import MainPage from './pages/Main';
 import SendAssetsPage from './pages/Send';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage /> */}
-
-      {/* <MainPage /> */}
-
-      <SendAssetsPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path="send" element={<SendAssetsPage />} />
+          <Route path="*" element={<div>Page not found 404</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
